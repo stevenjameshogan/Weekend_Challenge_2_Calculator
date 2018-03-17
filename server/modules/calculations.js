310b27583661f3bc6ec224080e586bca5879fcdf
Steve Hogan <stevenjameshogan@gmail.com>
2018-03-17 15:50:03 -0500

    Commit 12. Added basic styling to page

--- server/modules/calculations.js
+++ server/modules/calculations.js
@@ -1,12 +1,11 @@
 let addVals = require('./modules/addValues.js')
-let subVals = require('./modules/subtractValues.js')
-let multVals = require('./modules/multiplyValues.js')
-let divVals = require('./modules/divideValues.js')
+let subtractVals = require('./modules/subtractValues.js')
+let multiplyVals = require('./modules/multiplyValues.js')
+let divideVals = require('./modules/divideValues.js')
+// let percentVals = require('./modules/percentValues.js')
 
-let newCalc = req.body; // req.body is our 'calcInputs' object from client
-  let val1 = parseInt(newCalc.value1);
-  let val2 = parseInt(newCalc.value2);
-  switch(newCalc.calcType) { // routes to proper operation based on 'calcType'
+
+switch(newCalc.calcType) { // routes to proper operation based on 'calcType'
     case '+':
       addVals(val1, val2);
     break;
---
+++ server/modules/percentValues.js
@@ -0,0 +1,7 @@
+function percentVals(x) { // adds user inputs
+    
+    totalCalculation = x * 0.1;
+    return totalCalculation;
+  }
+
+module.exports = percentVals;
