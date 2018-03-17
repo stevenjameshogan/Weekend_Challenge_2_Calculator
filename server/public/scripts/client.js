a8c86cbaf5f79e2bd4fb464441f3c0e2d06117f1
Steve Hogan <stevenjameshogan@gmail.com>
2018-03-16 21:49:05 -0500

    Commit 3. Inputs mappes and POST req complete on both sides

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,5 +3,79 @@ $(readyNow);
 
 function readyNow() {
     console.log('jQuery sourced');
-    
+    eventHandlers();
+
 }
+
+function eventHandlers() { 
+    $('#addBtn').on('click', addVals);
+    $('#subtractBtn').on('click', subtractVals);
+    $('#multiplyBtn').on('click', multiplyVals);
+    $('#divideBtn').on('click', divideVals);
+}
+
+function addVals() {
+    let val1In = $('#value1In').val();
+    let val2In = $('#value2In').val();
+    let calcTypeIn = 'add';
+    let calcInputs = {value1: val1In, value2: val2In, calcType: calcTypeIn};
+    console.log(calcInputs);
+    $.ajax({
+        type: 'POST',
+        data: calcInputs,
+        url: '/total'
+    }).done(function(response) {
+        console.log('hell yes');
+    }).fail(function(response) {
+        console.log('Noooooooo');
+    })
+}
+function subtractVals() {
+    let val1In = $('#value1In').val();
+    let val2In = $('#value2In').val();
+    let calcTypeIn = 'subtract';
+    let calcInputs = {value1: val1In, value2: val2In, calcType: calcTypeIn};
+    console.log(calcInputs);
+    $.ajax({
+        type: 'POST',
+        data: calcInputs,
+        url: '/total'
+    }).done(function(response) {
+        console.log('hell yes');
+    }).fail(function(response) {
+        console.log('Noooooooo');
+    })
+}
+function multiplyVals() {
+    let val1In = $('#value1In').val();
+    let val2In = $('#value2In').val();
+    let calcTypeIn = 'multiply';
+    let calcInputs = {value1: val1In, value2: val2In, calcType: calcTypeIn};
+    console.log(calcInputs);
+    $.ajax({
+        type: 'POST',
+        data: calcInputs,
+        url: '/total'
+    }).done(function(response) {
+        console.log('hell yes');
+    }).fail(function(response) {
+        console.log('Noooooooo');
+    })
+}
+function divideVals() {
+    let val1In = $('#value1In').val();
+    let val2In = $('#value2In').val();
+    let calcTypeIn = 'divide';
+    let calcInputs = {value1: val1In, value2: val2In, calcType: calcTypeIn};
+    console.log(calcInputs);
+    $.ajax({
+        type: 'POST',
+        data: calcInputs,
+        url: '/total'
+    }).done(function(response) {
+        console.log('hell yes');
+    }).fail(function(response) {
+        console.log('Noooooooo');
+    })
+}
+  
--- server/server.js
+++ server/server.js
@@ -2,12 +2,16 @@ let express = require('express');
 let app = express();
 let bodyParser = require('body-parser');
 const PORT = 3086;
+let calcHistory = [];
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('server/public'));
 
-
-
+app.post('/total', (req, res) => {
+  console.log(req.body);
+  let newCalc = req.body;
+  res.sendStatus(200);
+})
 
 
 
